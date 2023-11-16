ab73fcb0fa540504dc7531c44fcbadd4f2748875
chinobin <sfaigon@email.com>
2023-11-15 22:04:13 -0500

    complete part 2

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Car(models.Model):
+    name = models.CharField(max_length=100)
+    make = models.CharField(max_length=100)
+    model = models.CharField(max_length=100)
+    year = models.IntegerField()
+    color = models.CharField(max_length=100)
+    description = models.TextField(max_length=250)
+    def __str__(self):
+        return f'{self.year} {self.make} {self.model}'
--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import Car
 
 # Create your views here.
+def home(request):
+    return render(request, 'home.html')
+def about(request):
+    return render(request, 'about.html')
+def cars_index(request):
+    cars = Car.objects.all()
+    return render(request, 'cars/index.html', {
+        'cars': cars
+    })
+def cars_details(request, car_id):
+    car = Car.objects.get(id=car_id)
+    return render(request, 'cars/detail.html', {'car': car})
